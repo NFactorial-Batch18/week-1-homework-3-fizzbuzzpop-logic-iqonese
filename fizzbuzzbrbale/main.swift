db73d6aab09f702d669efc4a0c5cde1f2f713476
iqonese <iqonese@gmail.com>
2022-05-01 22:38:34 +0600

    Last project

--- fizzbuzzbrbale/main.swift
+++ fizzbuzzbrbale/main.swift
@@ -1,1 +1,27 @@
 import Foundation
+
+while(true) {
+    print("Please enter your number")
+    if let input = readLine() {
+        
+        let number = Int(input)!
+        if number % 105 == 0 {
+            print("FizzBuzzPop\n")
+        } else if number % 21 == 0 {
+            print("FizzPop\n")
+        } else if number % 15 == 0 {
+            print("FizzBuzz\n")
+        } else if number % 35 == 0 {
+            print("BuzzPop\n")
+        } else if number % 5 == 0 {
+            print("Buzz\n")
+        } else if (number % 3 == 0) {
+            print("Fizz\n")
+        } else if (number % 7 == 0) {
+            print("Pop\n")
+        } else {
+            print("\(number) \n")
+            
+        }
+    }
+}
